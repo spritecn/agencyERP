94e5dfb1848b0927c648257e469539f2d5107440
spx <spx@home>
2020-04-05 18:04:56 +0800

    init

---
+++ src/main/java/com/corse/agencyerp/AgencyerpApplication.java
@@ -0,0 +1,13 @@
+package com.corse.agencyerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyerpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgencyerpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/corse/agencyerp/AgencyerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corse.agencyerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencyerpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
